af78a202475faed35f668cb3939183b362f342a9
Jenith Jeyaranjan <jeyaranjanj@bzz.ch>
2022-05-23 21:52:12 +0200

    Add Comments

--- src/main/java/ch/bzz/pokemon/model/Pokemon.java
+++ src/main/java/ch/bzz/pokemon/model/Pokemon.java
@@ -2,6 +2,9 @@ package ch.bzz.pokemon.model;
 
 import ch.bzz.pokemon.data.DataHandler;
 
+/**
+ * a Pakemon
+ */
 public class Pokemon {
     private Typ typ;
     private Trainer trainer;
@@ -11,10 +14,18 @@ public class Pokemon {
     private Boolean megaEvolution;
     private Double groesse;
 
+    /**
+     * gets the typID from the Typ-object
+     * @return
+     */
     public String getTypID() {
         return getTyp().getTypID();
     }
 
+    /**
+     * creates a Typ-object without the pokemonList
+     * @param typID
+     */
     public void setTypID(String typID) {
         setTyp( new Typ());
         Typ typ = DataHandler.getInstance().readTypByID(typID);
@@ -22,19 +33,34 @@ public class Pokemon {
         getTyp().setTyp(typ.getTyp());
 
     }
-
+    /**
+     * gets typ
+     *
+     * @return value of typ
+     */
     public Typ getTyp() {
         return typ;
     }
 
+    /**
+     * sets typ
+     *
+     * @param typ the value to set
+     */
     public void setTyp(Typ typ) {
         this.typ = typ;
     }
-
+    /**
+     * gets the trainerID from the Trainer-object
+     * @return
+     */
     public String getTrainerID() {
         return getTrainer().getTrainerID();
     }
-
+    /**
+     * creates a Trainer-object without the pokemonList
+     * @param trainerID
+     */
     public void setTrainerID(String trainerID) {
         setTrainer( new Trainer());
         Trainer trainer = DataHandler.getInstance().readTrainerByID(trainerID);
@@ -42,15 +68,28 @@ public class Pokemon {
         getTrainer().setTrainer(trainer.getTrainer());
 
     }
-
+    /**
+     * gets trainer
+     *
+     * @return value of trainer
+     */
     public Trainer getTrainer() {
         return trainer;
     }
 
+    /**
+     * sets trainer
+     *
+     * @param trainer the value to set
+     */
     public void setTrainer(Trainer trainer) {
         this.trainer= trainer;
     }
-
+    /**
+     * gets trainer
+     *
+     * @return value of trainer
+     */
     public Boolean getMegaEvolution() {
         return megaEvolution;
     }
@@ -58,27 +97,51 @@ public class Pokemon {
     public void setMegaEvolution(Boolean megaEvolution) {
         this.megaEvolution = megaEvolution;
     }
-
+    /**
+     * gets groesse
+     *
+     * @return value of groesse
+     */
     public Double getGroesse() {
         return groesse;
     }
-
+    /**
+     * sets groesse
+     *
+     * @param groesse the value to set
+     */
     public void setGroesse(Double groesse) {
         this.groesse = groesse;
     }
-
+    /**
+     * gets pokemonID
+     *
+     * @return value of pokemonID
+     */
     public String getPokemonID() {
         return pokemonID;
     }
-
+    /**
+     * sets pokemonID
+     *
+     * @param pokemonID the value to set
+     */
     public void setPokemonID(String pokemonID) {
         this.pokemonID = pokemonID;
     }
-
+    /**
+     * gets name
+     *
+     * @return value of name
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * sets name
+     *
+     * @param name the value to set
+     */
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/ch/bzz/pokemon/model/Typ.java
+++ src/main/java/ch/bzz/pokemon/model/Typ.java
@@ -5,17 +5,40 @@ public class Typ {
     private String typID;
 
 
+    /**
+     * gets typ
+     *
+     * @return value of typ
+     */
     public String getTyp() {
         return typ;
     }
+
+    /**
+     * sets typ
+     *
+     * @param typ the value to set
+     */
 
     public void setTyp(String typ) {
         this.typ = typ;
     }
 
+    /**
+     * gets typID
+     *
+     * @return value of typiD
+     */
+
     public String getTypID() {
         return typID;
     }
+
+    /**
+     * sets publisherUUID
+     *
+     * @param typID the value to set
+     */
 
     public void setTypID(String typID) {
         this.typID = typID;
