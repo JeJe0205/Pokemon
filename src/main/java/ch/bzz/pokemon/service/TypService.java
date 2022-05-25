6a46f82f1130783dade7f3c5a857104a41775f13
Jenith Jeyaranjan <jeyaranjanj@bzz.ch>
2022-05-25 15:55:33 +0200

    Pokemon,Typ,Trainer DELETE+CREATE+UPDATE in DataHandler Hinzugefügt

--- src/main/java/ch/bzz/pokemon/service/TypService.java
+++ src/main/java/ch/bzz/pokemon/service/TypService.java
@@ -1,12 +1,11 @@
 package ch.bzz.pokemon.service;
 
 import ch.bzz.pokemon.data.DataHandler;
+import ch.bzz.pokemon.model.Pokemon;
 import ch.bzz.pokemon.model.Typ;
+import com.sun.xml.internal.bind.v2.model.core.ID;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -49,6 +48,83 @@ public class TypService {
         return Response
                 .status(httpsStatus)
                 .entity(typ)
+                .build();
+    }
+
+    /**
+     *
+     * @param name
+     * @param megaEvolution
+     * @param groesse
+     * @param trainerID
+     * @param typID
+     * @return
+     */
+    @POST
+    @Path("create")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response insertTyp(
+            @FormParam("typ") String typ,
+            @FormParam("typID") String typID
+    ){
+        Typ typ = new Typ();
+        typ.setTypID(ID.randomID().toString());
+        typ.setTyp(typ);
+        typ.setTypID(typID);
+
+
+        DataHandler.insertTyp(typ);
+        return Response
+                .status(200)
+                .entity("")
+                .build();
+    }
+
+    @POST
+    @Path("update")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response updateTyp(
+            @FormParam("typID") String typID,
+            @FormParam("typ") String typ
+
+    ){
+        int httpStatus = 200;
+        Typ typ = DataHandler.readTypByID(typID);
+        if (typ != null){
+            pokemon.setName(name);
+            pokemon.setMegaEvolution(megaEvolution);
+            pokemon.setGroesse(groesse);
+            pokemon.setTrainerID(trainerID);
+            pokemon.setTypID(typID);
+
+            DataHandler.updatePokemon();
+        }else {
+            httpStatus = 410;
+        }
+        return Response
+                .status(httpStatus)
+                .entity("")
+                .build();
+    }
+
+    /**
+     * delets a pokemon indentified by its uuid
+     * @param pokemonID the key
+     * @return Response
+     */
+    @DELETE
+    @Path("delete")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deletePokemon(
+            @QueryParam("id") String pokemonID
+    ){
+        int httpStatus = 200;
+        if (!DataHandler.deletePokemon(pokemonID)){
+            httpStatus = 410;
+        }
+        return Response
+                .status(httpStatus)
+                .entity("")
                 .build();
     }
 }
